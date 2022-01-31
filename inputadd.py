c1c9d1b7075ead65e494620fd19e36fac315aa8b
vidya ganesan <vidyaganesan15@gmail.com>
2022-01-31 22:55:29 +0400

    added inputadd.py

---
+++ inputadd.py
@@ -0,0 +1,7 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:\Users\User\Desktop\VC\scripts\add.py ===============
+Please add your name: Vidya Ganesan
+Please add your favourite sports name: Badminton
+Please add your name: 
